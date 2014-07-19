1cfd344beedc2940fc3de52ab20528324c430b91
Eslam A. Hefnawy <eahefnawy@gmail.com>
2014-07-19 20:22:47 +0300

    filled up package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: "Reaction Search - Search Feature For Your Reaction Shop"
+});
+
+Package.on_use(function (api, where) {
+  api.use([
+    "templating",
+    "coffeescript",
+    "iron-router",
+    "simple-schema",
+    "autoform",
+    "underscore-string-latest",
+    "less",
+    "reaction-core"
+  ], ["client", "server"]);
+  api.add_files([
+    "client/register.coffee",
+    "client/routing.coffee",
+    "client/templates/searchBox.html",
+    "client/templates/searchBox.coffee"
+  ], ["client"]);
